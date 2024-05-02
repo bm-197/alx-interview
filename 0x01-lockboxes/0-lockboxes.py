8c762ec014775f15b657f8dd98051c363c867cbd
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-02 10:21:45 +0300

    [Task-0] feat: Lock boxes solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 
+and each box may contain keys to the other boxes.
+"""
+
+def canUnlockAll(boxes):
+    """
+    A method that determines if all the boxes can be
+    opened.
+
+    Args:
+        boxes (List[List[int]]): the boxes holding the key
+    Return:
+        boolean: True if all can be open, False otherwise
+    """
+    from collections import deque
+
+    visted = set()
+    boxs = deque()
+    boxs.append(0)
+
+    while boxs:
+        box = boxs.popleft()
+
+        if box not in visted:
+            visted.add(box)
+            for key in boxes[box]:
+                boxs.append(key)
+
+    for i in range(len(boxes)):
+        if i not in visted:
+            return False
+    
+    return True #all boxes are opened
+
